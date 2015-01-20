b9c958e564735bd7ad75c1250404790be6c74adf
jake bolewski <jakebolewski@gmail.com>
2015-01-20 15:43:08 -0500

    refactor bunchkaufman

--- base/linalg/bunchkaufman.jl
+++ base/linalg/bunchkaufman.jl
@@ -7,33 +7,54 @@ immutable BunchKaufman{T,S<:AbstractMatrix} <: Factorization{T}
     ipiv::Vector{BlasInt}
     uplo::Char
     symmetric::Bool
-    BunchKaufman(LD::AbstractMatrix{T}, ipiv::Vector{BlasInt}, uplo::Char, symmetric::Bool) = new(LD, ipiv, uplo, symmetric)
-end
-BunchKaufman{T}(LD::AbstractMatrix{T}, ipiv::Vector{BlasInt}, uplo::Char, symmetric::Bool) = BunchKaufman{T,typeof(LD)}(LD, ipiv, uplo, symmetric)
 
-function bkfact!{T<:BlasReal}(A::StridedMatrix{T}, uplo::Symbol=:U, symmetric::Bool=issym(A))
-    symmetric || error("The Bunch-Kaufman decomposition is only valid for symmetric matrices")
+    BunchKaufman(LD::AbstractMatrix{T}, ipiv::Vector{BlasInt}, uplo::Char, symmetric::Bool) =
+        new(LD, ipiv, uplo, symmetric)
+end
+BunchKaufman{T}(LD::AbstractMatrix{T}, ipiv::Vector{BlasInt}, uplo::Char, symmetric::Bool) =
+    BunchKaufman{T,typeof(LD)}(LD, ipiv, uplo, symmetric)
+
+bkfact!{T<:BlasReal}(A::StridedMatrix{T}, uplo::Symbol=:U, symmetric::Bool=issym(A)) = begin
+    if !symmetric
+        error("The Bunch-Kaufman decomposition is only valid for symmetric matrices")
+    end
     LD, ipiv = LAPACK.sytrf!(char_uplo(uplo) , A)
     BunchKaufman(LD, ipiv, char_uplo(uplo), symmetric)
 end
-function bkfact!{T<:BlasComplex}(A::StridedMatrix{T}, uplo::Symbol=:U, symmetric::Bool=issym(A))
-    LD, ipiv = (symmetric ? LAPACK.sytrf! : LAPACK.hetrf!)(char_uplo(uplo) , A)
+
+bkfact!{T<:BlasComplex}(A::StridedMatrix{T}, uplo::Symbol=:U, symmetric::Bool=issym(A)) = begin
+    if symmetric
+        LD, ipiv = LAPACK.sytrf!(char_uplo(uplo), A)
+    else
+        LD, ipiv = LAPACK.hetrf!(char_uplo(uplo) , A)
+    end
     BunchKaufman(LD, ipiv, char_uplo(uplo), symmetric)
 end
-bkfact{T<:BlasFloat}(A::StridedMatrix{T}, uplo::Symbol=:U, symmetric::Bool=issym(A)) = bkfact!(copy(A), uplo, symmetric)
-bkfact{T}(A::StridedMatrix{T}, uplo::Symbol=:U, symmetric::Bool=issym(A)) = bkfact!(convert(Matrix{promote_type(Float32,typeof(sqrt(one(T))))},A),uplo,symmetric)
 
-convert{T}(::Type{BunchKaufman{T}},B::BunchKaufman) = BunchKaufman(convert(Matrix{T},B.LD),B.ipiv,B.uplo,B.symmetric)
-convert{T}(::Type{Factorization{T}}, B::BunchKaufman) = convert(BunchKaufman{T}, B)
+bkfact{T<:BlasFloat}(A::StridedMatrix{T}, uplo::Symbol=:U, symmetric::Bool=issym(A)) =
+    bkfact!(copy(A), uplo, symmetric)
+
+bkfact{T}(A::StridedMatrix{T}, uplo::Symbol=:U, symmetric::Bool=issym(A)) = begin
+    MT = promote_type(Float32, typeof(sqrt(one(T))))
+    bkfact!(convert(Matrix{MT}, A), uplo, symmetric)
+end
+
+convert{T}(::Type{BunchKaufman{T}}, B::BunchKaufman) =
+    BunchKaufman(convert(Matrix{T}, B.LD), B.ipiv, B.uplo, B.symmetric)
+
+convert{T}(::Type{Factorization{T}}, B::BunchKaufman) =
+    convert(BunchKaufman{T}, B)
 
 size(B::BunchKaufman) = size(B.LD)
-size(B::BunchKaufman,d::Integer) = size(B.LD,d)
+size(B::BunchKaufman, d::Integer) = size(B.LD,d)
+
 issym(B::BunchKaufman) = B.symmetric
 ishermitian(B::BunchKaufman) = !B.symmetric
 
-inv{T<:BlasReal}(B::BunchKaufman{T}) = copytri!(LAPACK.sytri!(B.uplo, copy(B.LD), B.ipiv), B.uplo, true)
+inv{T<:BlasReal}(B::BunchKaufman{T}) =
+    copytri!(LAPACK.sytri!(B.uplo, copy(B.LD), B.ipiv), B.uplo, true)
 
-function inv{T<:BlasComplex}(B::BunchKaufman{T})
+inv{T<:BlasComplex}(B::BunchKaufman{T}) = begin
     if issym(B)
         copytri!(LAPACK.sytri!(B.uplo, copy(B.LD), B.ipiv), B.uplo)
     else
@@ -41,7 +62,13 @@ function inv{T<:BlasComplex}(B::BunchKaufman{T})
     end
 end
 
-A_ldiv_B!{T<:BlasReal}(B::BunchKaufman{T}, R::StridedVecOrMat{T}) = LAPACK.sytrs!(B.uplo, B.LD, B.ipiv, R)
-function A_ldiv_B!{T<:BlasComplex}(B::BunchKaufman{T}, R::StridedVecOrMat{T})
-    (issym(B) ? LAPACK.sytrs! : LAPACK.hetrs!)(B.uplo, B.LD, B.ipiv, R)
+A_ldiv_B!{T<:BlasReal}(B::BunchKaufman{T}, R::StridedVecOrMat{T}) =
+    LAPACK.sytrs!(B.uplo, B.LD, B.ipiv, R)
+
+A_ldiv_B!{T<:BlasComplex}(B::BunchKaufman{T}, R::StridedVecOrMat{T}) = begin
+    if issym(B)
+        LAPACK.sytrs!(B.uplo, B.LD, B.ipiv, R)
+    else
+        LAPACK.hetrs!(B.uplo, B.LD, B.ipiv, R)
+    end
 end
